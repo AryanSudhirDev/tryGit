ebe10aa35de62d9804f1db70a0b5add6beb920a6
AryanSudhirDev <aryansudhir@icloud.com>
2024-12-08 14:11:26 -0800

    Initial Commit

---
+++ projectToDo/projectToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectToDoApp.swift
+//  projectToDo
+//
+//  Created by Aryan Sudhir on 12/8/24.
+//
+
+import SwiftUI
+
+@main
+struct projectToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projectToDoTests/projectToDoTests.swift
@@ -0,0 +1,17 @@
+//
+//  projectToDoTests.swift
+//  projectToDoTests
+//
+//  Created by Aryan Sudhir on 12/8/24.
+//
+
+import Testing
+@testable import projectToDo
+
+struct projectToDoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
